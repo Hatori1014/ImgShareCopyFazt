3b57f991e455243f2f4370276fed1ca5821d5d8c
Hatori1014 <wfarguelloc@gmail.com>
2019-10-29 00:00:26 -0500

    Agregando Funcionalidad para cargar Imagenes(function ctrl.create)

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,10 +1,24 @@
+const path = require('path');
+const fs = require('fs-extra');
+const helpers = require('../helpers/libs');
+
 const ctrl= {};
 
 ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = (req, res) => {
+ctrl.create = async (req, res) => {
+  const imgUrl = helpers.randomNumber();
+  console.log(imgUrl);
+  const imageTempPath = req.file.path; 
+  const ext = path.extname(req.file.originalname).toLowerCase();
+  const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`); 
+
+  if(ext ==='.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+    await fs.rename(imageTempPath, targetPath);
+  }
+  res.send('Works!');  
 
 };
 
