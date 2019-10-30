88fcb730f850dd3e66226bfc9769d0f084a443c2
wilsonRayco19 <wilson.arguello@rayco.co>
2019-10-30 17:21:02 -0500

    Creacion de Modelos, guardar Imgs en BD y Mostrar imagenes guardadas en la Vista index

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,10 @@
+const { Image } = require('../models');
+
 const ctrl = {};
 
-ctrl.index = (req, res) => {
-  res.render('index');
+ctrl.index = async (req, res) => {
+  const images = await Image.find().sort({timestamp: -1}); // manera ascendente = 1 , manera descendente = -1
+  res.render('index', { images }); //se puede reducir images: images a solo images
 
 };
 
--- src/controllers/image.js
+++ src/controllers/image.js
@@ -2,24 +2,46 @@ const path = require('path');
 const fs = require('fs-extra');
 const helpers = require('../helpers/libs');
 
-const ctrl= {};
+// en Node JS cuando se importa busca un archivo index, por consiguiente se puede omitir el index (require('../models/index') = require('../models')
+const { Image } = require('../models');
+
+const ctrl = {};
 
 ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = async (req, res) => {
-  const imgUrl = helpers.randomNumber();
-  console.log(imgUrl);
-  const imageTempPath = req.file.path; 
-  const ext = path.extname(req.file.originalname).toLowerCase();
-  const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`); 
+ctrl.create = (req, res) => {
 
-  if(ext ==='.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
-    await fs.rename(imageTempPath, targetPath);
-  }
-  res.send('Works!');  
+  const saveImage = async() => {
+    const imgUrl = helpers.randomNumber();
+    const images = await Image.find({ filename: imgUrl });
 
+    if (images.length > 0) {
+      imgUrl = helpers.randomNumber();
+      saveImage();
+    } else {
+      console.log(imgUrl);
+      const imageTempPath = req.file.path;
+      const ext = path.extname(req.file.originalname).toLowerCase();
+      const targetPath = path.resolve(`src/public/upload/${imgUrl}${ext}`);
+      if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
+        await fs.rename(imageTempPath, targetPath);
+        const newImg = new Image({
+          title: req.body.title,
+          filename: imgUrl + ext,
+          description: req.body.description,
+        });
+        const imageSaved = await newImg.save();
+        //res.redirect('/images/:image_id');
+        res.send('works!');
+      } else {
+          await fs.unlink(imageTempPath);
+          res.status(500).json({ error: 'Only Images are allowed' });
+      }
+    }
+  };
+  saveImage();
 };
 
 ctrl.like = (req, res) => {
